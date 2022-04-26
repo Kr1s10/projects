da1c17a74eb90e6850de14faab4c877ad49af034
Kristen <kr1s10.khud@gmail.com>
2022-04-26 07:36:04 +0300

    feat: add popup for main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -32,11 +32,14 @@ function close () {
 const sliderWrapper = document.querySelector('.slider-wrapper');
 const prevBtn = document.querySelector('.slider-btn-left');
 const nextBtn = document.querySelector('.slider-btn-right');
-let currWidth;
-let currCards = checkCurrWidth();
-let pets = [];
 
-await getPets(pets)
+let currWidth = 1280
+let currCards = checkCurrWidth()
+let pets = []
+let startPets = []
+    
+await preloadPets(startPets)
+
 showCards()
 
 prevBtn.addEventListener('click', leftMove);
@@ -48,7 +51,7 @@ window.addEventListener('resize', () => {
     }
 })
 
-async function getPets(arr) {
+async function preloadPets(arr) {
     const res = await fetch('../../assets/json/pets.json');
     const data = await res.json();
 
@@ -64,7 +67,7 @@ function createCard(index) {
     <div class="slider-card" data-pet="${index}">
         <img class="slider-photo" src="${pets[index].img}" alt="${pets[index].name}">
         <h3 class="slider-card-title">${pets[index].name}</h3>
-        <button class="slider-card-btn">Learn more</button>
+        <button class="slider-card-btn" data-btn="${index}">Learn more</button>
     </div>
     `
 }
@@ -181,4 +184,60 @@ function leftMove () {
     setTimeout(() => { showCards()}, 500);
 }
 
+// popup
+
+const popupWrapper = document.querySelector('.popup-wrapper');
+const popup = document.querySelector('.popup');
+
+sliderWrapper.addEventListener('click', (e) => {
+    if (e.target.classList.contains('slider-card-btn')) {
+        createPopup(e.target.dataset.btn);
+        document.body.classList.add('lock');
+        popupWrapper.classList.add('active');
+    }
+})
+
+popupWrapper.addEventListener('click', (e) => {
+    if (e.target.classList.contains('popup-wrapper') || e.target.classList.contains('popup-close-btn')) {
+        popupWrapper.classList.remove('active');
+        document.body.classList.remove('lock');
+        popup.innerHTML = '';
+    }
+})
+
+function createPopup(index) {
+    popup.insertAdjacentHTML('beforeend', `
+    <button class="popup-close-btn">
+        <img src="../../assets/icons/close.svg" alt="close">
+    </button>
+    <div class="popup-card">
+        <img src="${pets[index].bigImg}" alt="${pets[index].name}">
+        <div class="popup-content">
+            <div class="popup-title">
+                <h2>${pets[index].name}</h2>
+                <span>${pets[index].type} - ${pets[index].breed}</span>
+            </div>
+            <p class="popup-description">${pets[index].description}</p>
+            <ul class="popup-list">
+                <li class='popup-item'>
+                    <span class="category">Age:</span>
+                    ${pets[index].age}
+                </li>
+                <li class='popup-item'>
+                    <span class="category">Inoculations:</span>
+                    ${pets[index].inoculations.join(', ')}
+                </li>
+                <li class='popup-item'>
+                    <span class="category">Diseases:</span>
+                    ${pets[index].diseases.join(', ')}
+                </li>
+                <li class='popup-item'>
+                    <span class="category">Parasites:</span>
+                    ${pets[index].parasites.join(', ')}
+                </li>
+            </ul>
+        </div>
+    </div>
+    `)
+}
 
