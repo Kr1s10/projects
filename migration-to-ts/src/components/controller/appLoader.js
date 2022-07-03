28185bbe427b442360036ad850771b631505581d
Kristen <kr1s10.khud@gmail.com>
2022-07-03 15:53:45 +0300

    feat: add API key & base interfaces

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7f1121b9e0a24bb9b63c22d2a2c00d4d', // получите свой ключ https://newsapi.org/
         });
     }
 }
