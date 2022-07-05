4e49a9964578baac64136b341231322e82f16f6e
Kristen <kr1s10.khud@gmail.com>
2022-07-05 04:05:20 +0300

    refactor: change url for api

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '58f816e3091e43bba37302feb39b9be3', // получите свой ключ https://newsapi.org/
         });
     }
