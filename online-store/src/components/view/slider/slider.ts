c9b6b5c4b7f590ecb42d8ce90ff8148ada7a5960
Kristen <kr1s10.khud@gmail.com>
2022-07-17 17:56:27 +0300

    feat: add sliders

--- online-store/src/components/view/slider/slider.ts
+++ online-store/src/components/view/slider/slider.ts
@@ -2,33 +2,50 @@ import '../../../libs/nouislider/nouislider.min.css';
 import * as noUiSlider from 'nouislider';
 import './slider.scss';
 
-const priceSlider = document.querySelector('.slider-price__slider') as noUiSlider.target;
-const dateSlider = document.querySelector('.slider-date__slider') as noUiSlider.target;
+export class Slider {
+    public slider: noUiSlider.target;
+    private type: string;
+    private values: number[];
 
-noUiSlider.create(priceSlider, {
-    start: [0, 845],
-    connect: true,
-    range: {
-        min: 0,
-        max: 845,
-    },
-    step: 1,
-    format: {
-        from: (value) => parseInt(value),
-        to: (value) => Math.round(value),
-    },
-});
+    constructor(values: number[], type: string) {
+        this.values = values;
+        this.type = type;
+        this.slider = document.querySelector(`.slider-${type}__slider`) as noUiSlider.target;
+        this.create();
+    }
 
-noUiSlider.create(dateSlider, {
-    start: [2008, 2022],
-    connect: true,
-    range: {
-        min: 2008,
-        max: 2022,
-    },
-    step: 1,
-    format: {
-        from: (value) => parseInt(value),
-        to: (value) => Math.round(value),
-    },
-});
+    private create(): void {
+        noUiSlider.create(this.slider, {
+            start: this.values,
+            connect: true,
+            range: {
+                min: this.values[0],
+                max: this.values[1],
+            },
+            step: 1,
+            format: {
+                from: (value) => parseInt(value),
+                to: (value) => Math.round(value),
+            },
+        });
+        this.changeValues();
+    }
+
+    private changeValues(): void {
+        const inputs = [
+            document.querySelector(`#input-${this.type}_low`) as HTMLInputElement,
+            document.querySelector(`#input-${this.type}_high`) as HTMLInputElement,
+        ];
+
+        this.slider.noUiSlider?.on('update', (values, handle) => {
+            inputs[handle].value = values[handle].toString();
+        });
+
+        inputs.forEach((el, i) => {
+            el.addEventListener('change', (e) => {
+                this.values[i] = +(e.currentTarget as HTMLInputElement).value;
+                this.slider.noUiSlider?.set(this.values);
+            });
+        });
+    }
+}
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,7 +1,11 @@
 import './style.scss';
-import './components/view/slider/slider';
+import { Slider } from './components/view/slider/slider';
 import data from './data';
 import { Card } from './components/view/cards/cards';
+
+const priceSlider = new Slider([0, 845], 'price');
+
+const dateSlider = new Slider([2008, 2022], 'date');
 
 const app = new Card();
 app.draw(data);
